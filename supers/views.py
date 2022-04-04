b52735cffef767fc131f1e4a3f1b766670a8cc69
WTDula <wtdula@gmail.com>
2022-04-04 13:31:59 -0500

    added get_by_id functionality to supers_detail, added create_super functionality to supers_list

--- supers/views.py
+++ supers/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Super
+from .serializers import SuperSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 # Create your views here.
-@api_view("GET", "POST")
+@api_view(["GET", "POST"])
 def supers_list(request):
-    pass
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        serializer = SuperSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
 
-@api_view("GET", "PUT", "DELETE")
+@api_view(["GET", "PUT", "DELETE"])
 def supers_detail(request, pk):
-    pass
+    super = get_object_or_404(Super, pk = pk)
+    if request.method == "GET":
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+    
