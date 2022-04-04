b1ae59455ee27001897fa00e3725047a898db270
WTDula <wtdula@gmail.com>
2022-04-04 13:13:34 -0500

    added paths to heroes_villians_project, added serializers file to supers app, added paths to supers.urls.py, added function skeletons to supers.views.py

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view("GET", "POST")
+def supers_list(request):
+    pass
+
+@api_view("GET", "PUT", "DELETE")
+def supers_detail(request, pk):
+    pass
